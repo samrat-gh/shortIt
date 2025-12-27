d8a02fa875cad2ffa5fd4e6fce6d47b662642e7a
Samrat Ghimire <samratghimire32@gmail.com>
2025-12-27 20:46:03 +0545

    update utiluty to get server time

--- client/lib/getServerTime.ts
+++ client/lib/getServerTime.ts
@@ -1,7 +1,13 @@
-async function getServerTime = () => {
-    const res = await fetch('/api/now', {
-        method: "GET",
-        cache: 'no-cache'
-    })
-    return 
+async function getServerTime() {
+  const res = await fetch("/api/now", {
+    method: "GET",
+    cache: "no-cache",
+  });
+
+  if (!res.ok) {
+    console.log(res.status, res.statusText);
+  }
+  const response = await res.json();
+
+  return response;
 }
